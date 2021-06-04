ae6f7a8870c2693c7e7faa69300c6e5122181cfe
maximerauturier <maxime.rauturier@gmail.com>
2021-06-04 17:28:53 +0200

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3df02f727733676481ac3b8baba10a08'),
+    },
+  },
+});

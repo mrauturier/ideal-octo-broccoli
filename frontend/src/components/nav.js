093a4b19021fc0c05ceaf99c957221d964819b1e
maximerauturier <maxime.rauturier@gmail.com>
2021-06-05 16:50:18 +0200

    Update Nav

--- frontend/src/components/nav.js
+++ frontend/src/components/nav.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Link, StaticQuery, graphql } from "gatsby";
+import {Link, StaticQuery, graphql} from "gatsby";
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+
 
 const Nav = () => (
-  <StaticQuery
-    query={graphql`
+    <StaticQuery
+        query={graphql`
       query {
         strapiGlobal {
           siteName
@@ -18,41 +20,75 @@ const Nav = () => (
         }
       }
     `}
-    render={(data) => (
-      <div>
-        <div>
-          <nav className="uk-navbar-container" data-uk-navbar>
-            <div className="uk-navbar-left">
-              <ul className="uk-navbar-nav">
-                <li>
-                  <Link to="/">{data.strapiGlobal.siteName}</Link>
-                </li>
-              </ul>
+        render={(data) => (
+            <div>
+                <div>
+                    <nav className="bg-black" data-uk-navbar>
+                        <div className="text-white max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                            <div className="relative flex items-center justify-between h-16">
+                                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                    {/* Mobile menu button*/}
+                                    <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                        <span className="sr-only">Open main menu</span>
+                                        {open ? (
+                                            <XIcon className="block h-6 w-6" aria-hidden="true" />
+                                        ) : (
+                                            <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                                        )}
+                                    </button>
+                                </div>
+                                <div className="flex-1 flex items-center justify-center sm:justify-start">
+                                    <div className="flex-shrink-0 flex items-center">
+                                        <Link className="font-bold text-4xl" to="/">{data.strapiGlobal.siteName}</Link>
+                                    </div>
+                                    <div className="hidden sm:block sm:ml-6">
+                                        <div className="flex space-x-4">
+                                            {data.allStrapiCategory.edges.map((category) => (
+                                                <a
+                                                    key={`category__${category.node.slug}`}
+                                                    href={`/category/${category.node.slug}`}
+                                                    className={ 'bg-gray-900 text-white uppercase'}
+                                                    aria-current={category.current ? 'page' : undefined}
+                                                >
+                                                    {category.node.name}
+                                                </a>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            {/*<div className="uk-navbar-left">*/}
+                            {/*    <ul className="uk-navbar-nav">*/}
+                            {/*        <li>*/}
+                            {/*            <Link className="font-bold" to="/">{data.strapiGlobal.siteName}</Link>*/}
+                            {/*        </li>*/}
+                            {/*    </ul>*/}
+                            {/*</div>*/}
+                            {/*<div className="uk-navbar-right">*/}
+                            {/*    <button*/}
+                            {/*        className="uk-button uk-button-default uk-margin-right"*/}
+                            {/*        type="button"*/}
+                            {/*    >*/}
+                            {/*        Categories*/}
+                            {/*    </button>*/}
+                            {/*    <div uk-dropdown="animation: uk-animation-slide-top-small; duration: 1000">*/}
+                            {/*        <ul className="uk-nav uk-dropdown-nav">*/}
+                            {/*            {data.allStrapiCategory.edges.map((category, i) => (*/}
+                            {/*                <li key={`category__${category.node.slug}`}>*/}
+                            {/*                    <Link to={`/category/${category.node.slug}`}>*/}
+                            {/*                        {category.node.name}*/}
+                            {/*                    </Link>*/}
+                            {/*                </li>*/}
+                            {/*            ))}*/}
+                            {/*        </ul>*/}
+                            {/*    </div>*/}
+                            {/*</div>*/}
+                        </div>
+                    </nav>
+                </div>
             </div>
-            <div className="uk-navbar-right">
-              <button
-                className="uk-button uk-button-default uk-margin-right"
-                type="button"
-              >
-                Categories
-              </button>
-              <div uk-dropdown="animation: uk-animation-slide-top-small; duration: 1000">
-                <ul className="uk-nav uk-dropdown-nav">
-                  {data.allStrapiCategory.edges.map((category, i) => (
-                    <li key={`category__${category.node.slug}`}>
-                      <Link to={`/category/${category.node.slug}`}>
-                        {category.node.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </div>
-    )}
-  />
+        )}
+    />
 );
 
 export default Nav;
--- frontend/src/components/seo.js
+++ frontend/src/components/seo.js
@@ -89,20 +89,7 @@ const SEO = ({ seo = {} }) => {
         {
           rel: "stylesheet",
           href:
-            "https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css",
-        },
-      ]}
-      script={[
-        {
-          src:
-            "https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js",
-        },
-        {
-          src:
-            "https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js",
-        },
-        {
-          src: "https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js",
+            "https://maxst.icons8.com/vue-static/landings/line-awesome/font-awesome-line-awesome/css/all.min.css",
         },
       ]}
       meta={metaTags}
